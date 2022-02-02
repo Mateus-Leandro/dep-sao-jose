09bcb2fe71db5c4ef1f4051ec9dfadf36ea2da58
Mateus Leandro Chagas Andrade <mateusleandro2205@gmail.com>
2022-02-02 12:50:18 -0300

    Ajuste no sorter monetário para ordenar Doubles maiores que 999 presentes nas tabelas.

--- src/tables/tableSorters/SorterMonetario.java
+++ src/tables/tableSorters/SorterMonetario.java
@@ -8,8 +8,8 @@ public class SorterMonetario implements Comparator<String>{
 	@Override
 	public int compare(String valor1, String valor2) {
 		
-		Double formatado1 = Double.parseDouble(valor1.substring(3, valor1.length()).replaceAll(",", "."));
-		Double formatado2 = Double.parseDouble(valor2.substring(3, valor2.length()).replaceAll(",","."));
+		Double formatado1 = Double.parseDouble(valor1.substring(3, valor1.length()).replaceAll("\\.", "").replaceAll(",", "."));
+		Double formatado2 = Double.parseDouble(valor2.substring(3, valor2.length()).replaceAll("\\.", "").replaceAll(",","."));
 		
 		
 		return formatado1.compareTo(formatado2);
