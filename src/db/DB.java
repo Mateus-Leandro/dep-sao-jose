239403002dd5c92479459d2b63361239c8cc438b
Mateus Leandro Chagas Andrade <mateusleandro2205@gmail.com>
2022-12-07 19:58:26 -0300

    Alteração na função de conexão com o banco.

--- src/db/DB.java
+++ src/db/DB.java
@@ -7,28 +7,30 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Base64;
 import java.util.Properties;
 
 import javax.swing.JOptionPane;
 
-import org.apache.commons.dbcp.BasicDataSource;
-
 import tools.Log_tools;
-
 
 public class DB {
 
 	private static Connection conn = null;
 	private static Boolean reconectar;
-	private static String url;
 	private static Properties props;
-	private static BasicDataSource dataSource;
 	private static Log_tools log_tools = new Log_tools();
-	
+
+	private static String usuario;
+	private static String senha;
+	private static Integer NS; // Numero de série do cliente.
+	private static String ipBanco;
+	private static String nomeBanco;
+	private static String url;
+
 	// Busca conexão com o banco de dados.
 	public static Connection getConnection() {
 
@@ -47,18 +49,19 @@ public class DB {
 				if (conn == null || conn.isClosed()) {
 					try {
 						props = carregarDados();
-						url = props.getProperty("dburl");
+						verifica_serie();
+						url = "jdbc:mysql://" + ipBanco + ":3306" + "/" + nomeBanco
+								+ "?allowPublicKeyRetrieval=true&useSSL=false&useTimezone=true&serverTimezone=UTC";
 
-						// Busca conexão do pool
-						conn = getDataSource().getConnection();
+						conn = DriverManager.getConnection(url, usuario, senha);
+
 					} catch (SQLException e) {
 						e.printStackTrace();
 						log_tools.grava_log_error("DB.java", "getConnection" + " | [Erro de conexão]", e);
 
 						int opcao = JOptionPane.showConfirmDialog(null,
 								"Erro na conexão com o banco dados.\nDeseja tentar reconectar?",
-								"Conexão com o banco de dados.", JOptionPane.YES_OPTION,
-								JOptionPane.WARNING_MESSAGE);
+								"Conexão com o banco de dados.", JOptionPane.YES_OPTION, JOptionPane.WARNING_MESSAGE);
 
 						reconectar = opcao == JOptionPane.YES_OPTION;
 
@@ -76,7 +79,7 @@ public class DB {
 		}
 	}
 
-	// Lê os dados de conexão do banco atrav�s de um arquivo.
+	// Lê os dados de conexão do banco através de um arquivo.
 	private static Properties carregarDados() {
 		try (FileInputStream fs = new FileInputStream("C:/dep/conf/db.properties")) {
 			Properties props = new Properties();
@@ -107,7 +110,7 @@ public class DB {
 		}
 	}
 
-	// Fecha conex�o com o banco.
+	// Fecha conexão com o banco.
 	public static void closeConnection(Connection conn) {
 		try {
 			if (conn != null || !conn.isClosed()) {
@@ -126,19 +129,9 @@ public class DB {
 		try {
 
 			BufferedWriter saida = new BufferedWriter(new FileWriter(path));
-			saida.write("useTimezone=true");
+			saida.write("NS=");
 			saida.newLine();
-			saida.write("serverTimezone=UTC");
-			saida.newLine();
-			saida.write("user=USUARIO_DO_BANCO");
-			saida.newLine();
-			saida.write("password=SENHA_DO_USUARIO");
-			saida.newLine();
-			saida.write("dburl=jdbc:mysql://IP_DO_BANCO:3306/banco_deposito");
-			saida.newLine();
-			saida.write("useSSL=false");
-			saida.newLine();
-			saida.write("allowPublicKeyRetrieval=true");
+			saida.write("ipBd=");
 			saida.close();
 
 		} catch (IOException e) {
@@ -147,35 +140,25 @@ public class DB {
 		}
 
 		JOptionPane.showMessageDialog(null, "O arquivo C:/dep/conf/db.properties"
-				+ ", necess�rio para a conex�o com o banco de dados, n�o foi encontrado ou est� com as configura��es inv�lidas."
-				+ "\nGentileza verificar as informa��es presentes dentro do arquivo.",
-				"Arquivo de conex�o inv�lido ou n�o encontrado.", JOptionPane.WARNING_MESSAGE);
+				+ ", necessário para a conexão com o banco de dados, não foi encontrado ou está com as configurações inválidas."
+				+ "\nGentileza verificar as informações presentes dentro do arquivo.",
+				"Arquivo de conexão inválido ou não encontrado.", JOptionPane.WARNING_MESSAGE);
 
 		System.exit(0);
 	}
-	
-	
-	// pool para controle de conex�es com o banco de dados.
-	public static BasicDataSource getDataSource () {
-		if(dataSource == null) {
-			BasicDataSource ds = new BasicDataSource();
-			ds.setUrl(url);
-			ds.setUsername(props.getProperty("user"));
-			ds.setPassword(decript(props.getProperty("password")));
-			ds.setMinIdle(5);
-			ds.setMaxIdle(10);
-			ds.setMaxOpenPreparedStatements(50);
-			dataSource = ds;
-		}
-		
-		return dataSource;
-	}
-	
-	//Descriptografa a senha do arquivo db.properties,
-	//utilizando base64 de forma a igonarar os 2 primeiro e 2 ultimos caracteres.
-	public static String decript(String pValor) {
-		pValor = pValor.substring(2, pValor.length() - 2);
-	    return new String(Base64.getDecoder().decode(pValor.getBytes()));
-	}
 
+	// Pega dados de acesso pelo número de serie do cliente.
+	public static void verifica_serie() {
+		NS = Integer.parseInt(props.getProperty("NS"));
+		ipBanco = props.getProperty("ipBd");
+
+		switch (NS) {
+			case 1:
+				usuario = "dep";
+				nomeBanco = "banco_deposito";
+				senha = "dep@saojose";
+				break;
+		}
+
+	}
 }
